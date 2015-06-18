9746504c67f773c643cfe0dc76b321263552d96a
josevavia <josevavia@josevavia.es>
2015-06-18 19:36:42 +0200

    Actualización archivos según curso

---
+++ app/Http/Middleware/TestMiddleware.php
@@ -0,0 +1,21 @@
+<?php namespace laravelsamples\Http\Middleware;
+
+use Closure;
+
+class TestMiddleware {
+
+	/**
+	 * Handle an incoming request.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @param  \Closure  $next
+	 * @return mixed
+	 */
+	public function handle($request, Closure $next)
+	{
+        print_r($request);
+
+		return $next($request);
+	}
+
+}
---
+++ app/Providers/TestProvider.php
@@ -0,0 +1,27 @@
+<?php namespace laravelsamples\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class TestProvider extends ServiceProvider {
+
+	/**
+	 * Bootstrap the application services.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		//
+	}
+
+	/**
+	 * Register the application services.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+}
